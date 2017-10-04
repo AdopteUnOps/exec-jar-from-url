8aa7f7865a2d0620ade57fd7f38a01b3deef556b
Jean-Louis Boudart <jeanlouis.boudart@gmail.com>
2017-10-04 19:24:32 +0200

    allow forwarding

--- startup.sh
+++ startup.sh
@@ -3,6 +3,6 @@ set -x
 set -e
 
 echo "Downloading ${SERVICE_JAR_URL}"
-curl ${SERVICE_JAR_URL} > /tmp/application.jar
+curl -L ${SERVICE_JAR_URL} > /tmp/application.jar
 
 java -jar /tmp/application.jar ${SERVICE_JAVA_ARGUMENTS}
